8933b858c15cca378f33529e1c22186b47616ffb
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-01-08 23:20:54 +0700

    Proxies must be handled before body-parser

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,15 @@ const main = async () => {
   try {
     const server = express()
 
-    expressLoader(server)
-    const nextApp = await nextLoader()
-    passportLoader(server)
-
     server.use('/api', apiRouter)
     server.use('/', rootRouter)
+
+    // apiRouter must be above expressLoader
+    // aka proxies must be handled before body-parser
+    expressLoader(server)
+    passportLoader(server)
+
+    const nextApp = await nextLoader()
     server.get('*', nextApp.getRequestHandler())
 
     server.listen(PORT, (err) => {
